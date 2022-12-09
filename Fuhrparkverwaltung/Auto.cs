7cf4eba9eec30200d7bbf858fed464ab2fab7ca4
Ugur Günay <ugurg3@gso.schule.koeln>
2022-12-09 09:13:32 +0100

    Code (in bearbeitung)

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+
+        private int kilometerstand;
+
+        public int    Kilometerstand
+        {
+            get
+            {
+
+                return kilometerstand;
+         
+            }
+        }
+
+
+        public Auto (int kilometerstand)
+        {
+
+            kilometerstand = this.kilometerstand;
+        }
+
+    }
+}
